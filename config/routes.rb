abbb4a9acb72e3ea2d5fa7b0d4c67208655c362d
Gil Yoffe <gil.yoffe@wework.com>
2016-11-15 14:49:43 +0200

    Route - root to restaurants index

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  root :to => "restaurants#index"
+  resources :restaurants do
+  end
 end

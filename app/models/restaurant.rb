fee4280396001aa71713a8687e52db275efdd492
Gil Yoffe <gil.yoffe@wework.com>
2016-11-15 14:47:18 +0200

    Added rating range validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,11 +7,9 @@ class Restaurant < ActiveRecord::Base
   DELIVERY_TIME = {thirty_minutes: 0, thirty_to_hour: 1, hour_or_more: 2}
 
   validates_presence_of :name, :cuisine, :rating, :delivery_time
+  validates_inclusion_of :rating, :in => 0..3
 
   scope :with_cuisine, -> (cuisine) { where(:cuisine => cuisine) }
   scope :with_rating, -> (rating) { where(:rating => rating) }
   scope :with_delivery_time, -> (delivery_time) { where(:delivery_time => delivery_time) }
-
-  attr_accessible :name, :cuisine, :rating, :delivery_time, :is_10bis_accepted
-
 end
